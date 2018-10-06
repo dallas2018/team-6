5d29ddd79fc92ff1a41531462ae929d03844310d
Saman Attar <saman.attar@outlook.com>
2018-10-06 05:51:24 -0500

    Run on local machine

--- app.py
+++ app.py
@@ -96,4 +96,5 @@ def apply():
     return render_template('apply.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=8000,debug=True)
+    app.run(
+    host='0.0.0.0',port=8000,debug=True)
