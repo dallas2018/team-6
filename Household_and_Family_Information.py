45f9d16f28bc4bdf6119b61dd52797f28e8846a4
Saman Attar <saman.attar@outlook.com>
2018-10-06 04:40:47 -0500

    Add the third set of forms

--- Household_and_Family_Information.py
+++ Household_and_Family_Information.py
@@ -2,7 +2,6 @@ from wtforms import Form, BooleanField, StringField, validators, SelectField, In
 
 
 class Household_and_Family_Information(Form):
-
     maritalStatus = SelectField('Marital Status *', choices=[('1','Please select...'), ('2','Single'), ('3','Married'),('4','Divorced'),('5','Separated'), ('6','Widow/Widower')])
     householdAnnualIncome = SelectField("What is your household's annual income? *", choices=[('1','Please select...'), ('2', 'Below $5,000'), ('3', '$5,000 - 10,000'),('4', '$10,001 - 15,000'),('5', '$15,001 - 20,000'),('6', '$20,001 - 25,000'),('7', '$25,001 - 30,000'), ('8', '$30,001 - 35,000'),('9', '$35,001 - 40,000'), ('10', '$40,001 - 45,000'),('11', '$45,001 - 50,000')])
     childrenUnder18 = IntegerField('How many children 17 years old and under live in your household? *', [validators.Length(min=0, max=120)])
