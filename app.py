e7ef1bb59f79de89d7a8884469d7d3e3ca4fd4e9
marcusnuyda <42593433+marcusnuyda@users.noreply.github.com>
2018-10-06 02:39:23 -0500

    Add /Modal1

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def page1():
     form = Contact_Information(request.form)
 
     return render_template('page1.html', form=form)
-
+@app.route('/Modal1')
+def Modal1():
+    return render_template('Modal1.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=8000,debug=True)
