aa2d1345d33cd17f01e6bc5d977389fa7c10b82a
Anthony Nguyen <anthony_nguyen101@yahoo.com>
2018-10-06 02:23:28 -0500

    Implemented Modal

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask('__name__')
 def index():
     return render_template('home.html')
 
+@app.route('/Modal1')
+def Modal1():
+    return render_template('Modal1.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=8000,debug=True)
